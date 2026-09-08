7f1a0e81ef8ace3e1deae2c344d404d25d8a9e16
Achilles29 <you@example.com>
2026-09-08 21:52:11 +0700

    update

--- lib/src/screens/printer_settings_screen.dart
+++ lib/src/screens/printer_settings_screen.dart
@@ -266,7 +266,7 @@ class _PrinterSettingsScreenState extends State<PrinterSettingsScreen> {
                       SelectableText(previewLines),
                     ],
                   ),
-                ),
+                ],
               ),
               actions: [
                 TextButton(
